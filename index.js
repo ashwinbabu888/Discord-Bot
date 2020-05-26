8856a84358366a5baf8f75db02ec4227a147d1b9
Ashwin Babu <60725231+ashwinbabu888@users.noreply.github.com>
2020-05-26 02:45:45 -0700

    initialized index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NzE0NzY4Mzc0MjI3NDAyNzcy.Xszg3g.bVtnJaMk3mDyYNKDCJjJ3B1iMXk';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "we in?"){
+        msg.reply('We\'re in.');
+    }
+})
+
+bot.login(token);

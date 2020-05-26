082d69b9e1590bb2bb1ad88fc8f7751b8efe6571
Ashwin Babu <ashwinbabu888@gmail.com>
2020-05-26 03:17:59 -0700

    changed message

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const bot = new Discord.Client();
 
 const token = 'NzE0NzY4Mzc0MjI3NDAyNzcy.Xszg3g.bVtnJaMk3mDyYNKDCJjJ3B1iMXk';
 
+const PREFIX = '!';
+
 bot.on('ready', () =>{
     console.log('This bot is online!');
 })
 
-bot.on('message', msg=>{
-    if(msg.content === "we in?"){
-        msg.reply('We\'re in!');
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.reply('pong!');
+        break;
     }
 })
 

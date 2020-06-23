878d232485b39f737fbb89299974efe8225be130
Ashwin Babu <60725231+ashwinbabu888@users.noreply.github.com>
2020-06-23 16:11:42 -0700

    moved functions to cogs

--- bot.py
+++ bot.py
@@ -33,34 +33,10 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-# clear function
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-
-
 # returns the user's ping
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Your ping is {round(client.latency * 1000)}ms')
-
-
-# 8 ball feature which responds to a question
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.",
-                 "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
-                 "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.",
-                 "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.",
-                 "Outlook not so good.", "Very doubtful."]
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-
-
-# echoes the given message and deletes the command
-@client.command(aliases=['copy'])
-async def echo(ctx, *, message):
-    await ctx.channel.purge(limit=1)
-    await ctx.send(f'{message}')
 
 
 # runs the client using its Discord API Token
